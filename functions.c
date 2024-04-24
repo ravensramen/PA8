dd371adb3d8b4ad0e04c4ca5a7c0d72246f1ec79
ravensramen <153202736+ravensramen@users.noreply.github.com>
2024-04-23 21:53:32 -0700

    functions.c

--- functions.c
+++ functions.c
@@ -2,17 +2,19 @@
 
 //QUESTION 1
 
+
+
 char* my_str_n_cat(char* destination, char* source, int n)
 {
-    
-        int i = 0;
-        while (source[i] != '\0' && i < n)
-        {
-            destination[i] = source[i];
-            i++;
-        }
-        destination[i] = '\0';
-        return destination;
+
+    int i = 0;
+    while (source[i] != '\0' && i < n)
+    {
+        destination[i] = source[i];
+        i++;
+    }
+    destination[i] = '\0';
+    return destination;
 
 }
 
@@ -89,14 +91,14 @@ int is_palindrome(char* string, int length) {
     // Check if the first and last characters are the same
     if (string[0] == string[length - 1]) {
         // Recursively check the substring excluding the first and last characters
-    is_palindrome(string + 1, length - 2);
+        is_palindrome(string + 1, length - 2);
     }
     else {
         // If the first and last characters are not the same, it's not a palindrome
         return 0;
     }
 }
-int sum_primes(unsigned int n) 
+int sum_primes(unsigned int n)
 {
     if (n < 2) {
         return 0; // Base case: return 0 for numbers less than 2
@@ -126,14 +128,14 @@ int is_prime(unsigned int num, unsigned int divisor) {
     }
 }
 
-int maximum_occurrences(char *string, Occurrences *struct_ptr, int* ptr, char* c_ptr)
+int maximum_occurrences(char* string, Occurrences* struct_ptr, int* ptr, char* c_ptr)
 {
     int i = 0, index = 0, frequency = 0;
-    char c = *(string + i);
+    char c = *(string + i);//character found at each index value of the string
 
     char* alphabets = "abcdefghijklmnopqrstuvwxyz";
 
-    while (c != '/0') //while not at the end of the string
+    while (c != '\0') //while not at the end of the string
     {
         switch (c)
         {
@@ -143,11 +145,11 @@ int maximum_occurrences(char *string, Occurrences *struct_ptr, int* ptr, char* c
             break;
         case 'b':
         case 'B':
-            (struct_ptr+1)->frequency++;
+            (struct_ptr + 1)->frequency++;
             break;
         case 'c':
         case 'C':
-            (struct_ptr+2)->frequency++;
+            (struct_ptr + 2)->frequency++;
             break;
         case 'd':
         case 'D':
@@ -173,23 +175,104 @@ int maximum_occurrences(char *string, Occurrences *struct_ptr, int* ptr, char* c
         case 'I':
             (struct_ptr + 8)->frequency++;
             break;
+        case 'j':
+        case 'J':
+            (struct_ptr + 9)->frequency++;
+            break;
+        case 'k':
+        case 'K':
+            (struct_ptr + 9)->frequency++;
+            break;
+        case 'l':
+        case 'L':
+            (struct_ptr + 11)->frequency++;
+            break;
 
+        case 'm':
+        case 'M':
+            (struct_ptr + 11)->frequency++;
+            break;
+
+        case 'n':
+        case 'N':
+            (struct_ptr + 13)->frequency++;
+            break;
+
+        case 'o':
+        case 'O':
+            (struct_ptr + 14)->frequency++;
+            break;
+
+        case 'p':
+        case 'P':
+            (struct_ptr + 15)->frequency++;
+            break;
+
+        case 'q':
+        case 'Q':
+            (struct_ptr + 16)->frequency++;
+            break;
+
+        case 'r':
+        case 'R':
+            (struct_ptr + 17)->frequency++;
+            break;
+
+        case 's':
+        case 'S':
+            (struct_ptr + 18)->frequency++;
+            break;
+
+        case 't':
+        case 'T':
+            (struct_ptr + 19)->frequency++;
+            break;
+
+        case 'u':
+        case 'U':
+            (struct_ptr + 20)->frequency++;
+            break;
+
+        case 'v':
+        case 'V':
+            (struct_ptr + 21)->frequency++;
+            break;
+
+        case 'w':
+        case 'W':
+            (struct_ptr + 22)->frequency++;
+            break;
+
+        case 'x':
+        case 'X':
+            (struct_ptr + 23)->frequency++;
+            break;
+
+        case 'y':
+        case 'Y':
+            (struct_ptr + 24)->frequency++;
+            break;
+
+        case 'z':
+        case 'Z':
+            (struct_ptr + 25)->frequency++;
+            break;
         }
         i++;
-        c = *(string + i);
+        c = *(string + i); //go on to next index value
     }
     frequency = struct_ptr->frequency; //access frequency in struct
 
-    for(i = 0; i<26; i++)
+    for (i = 0; i < 26; i++)
     {
-    if(frequency<(struct_ptr + i) ->frequency)
-    {
-        index = i;
-        frequency = (struct_ptr + i)->frequency;
+        if (frequency < (struct_ptr + i)->frequency)
+        {
+            index = i;
+            frequency = (struct_ptr + i)->frequency;
+        }
     }
-    }
-    *ptr = frequency;
-    *c_ptr = *(alphabets + index);
+    *ptr = frequency; //finds frequency of most occuring letter
+    *c_ptr = *(alphabets + index); //dereferences the alphabet character that was found the most amount of times
 
-
+    return frequency;
 }
